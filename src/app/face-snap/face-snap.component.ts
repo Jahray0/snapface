12b16bf2b36ac0f113d28f9629a48b996233d07c
Jahray0 <jaegle.maxime@gmail.com>
2023-06-29 10:44:09 +0200

    v 1.0.3

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -12,12 +12,36 @@ export class FaceSnapComponent implements OnInit{
   snaps!:number;
   imageUrl!:string;
 
+  title2!: string;
+  description2!: string;
+  creatDate2!: Date;
+  snaps2!:number;
+  imageUrl2!:string;
+
+  title3!: string;
+  description3!: string;
+  creatDate3!: Date;
+  snaps3!:number;
+  imageUrl3!:string;
+
   ngOnInit() {
-    this.title='Pikachu'
+    this.title='Pichu'
     this.description='Petite souris jaune électrique'
     this.creatDate=new Date();
     this.snaps=25;
-    this.imageUrl='https://www.pokebip.com/pokedex-images/ecarlate-violet/300/25.png?v=ev2'
+    this.imageUrl='https://www.pokebip.com/pokedex-images/ecarlate-violet/300/172.png?v=ev2'
+
+    /*this.title2='Pikachu'
+    this.description2='Évolution de Pichu'
+    this.creatDate2=new Date();
+    this.snaps2=32;
+    this.imageUrl2='https://www.pokebip.com/pokedex-images/ecarlate-violet/300/25.png?v=ev2'
+
+    this.title3='Raichu'
+    this.description3='Évolution de Pikachu'
+    this.creatDate3=new Date();
+    this.snaps3=50;
+    this.imageUrl3='https://www.pokebip.com/pokedex-images/ecarlate-violet/300/26.png?v=ev2'*/
   }
 
 
