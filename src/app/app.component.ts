a6dc8226ff08f99de9fcef6a0d97907fae7fe486
Jahray0 <jaegle.maxime@gmail.com>
2023-06-29 14:35:14 +0200

    v 1.1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from './models/face-snap.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements  OnInit {
+  mySnap!:FaceSnap;
+  mySnap2!:FaceSnap;
+  mySnap3!:FaceSnap;
+
+  ngOnInit() {
+    this.mySnap=new FaceSnap('Pichu', 'Petite souris jaune électrique', new Date(), 25, 'https://www.pokebip.com/pokedex-images/ecarlate-violet/300/172.png?v=ev2');
+    this.mySnap2=new FaceSnap('Pikachu', 'Évolution de Pichu', new Date(), 25, 'https://www.pokebip.com/pokedex-images/ecarlate-violet/300/25.png?v=ev2');
+    this.mySnap3=new FaceSnap('Raichu', 'Évolution de Raichu', new Date(), 25, 'https://www.pokebip.com/pokedex-images/ecarlate-violet/300/26.png?v=ev2');
+  }
+
   title = '';
+
 }
--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
 
 @Component({
   selector: 'app-face-snap',
@@ -6,50 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./face-snap.component.scss']
 })
 export class FaceSnapComponent implements OnInit{
-  title!:string;
-  description!:string;
-  creatDate!:Date;
-  snaps!:number;
-  imageUrl!:string;
 
-  title2!: string;
-  description2!: string;
-  creatDate2!: Date;
-  snaps2!:number;
-  imageUrl2!:string;
+  @Input() faceSnap!:FaceSnap;
+    buttonText!:string;
 
-  title3!: string;
-  description3!: string;
-  creatDate3!: Date;
-  snaps3!:number;
-  imageUrl3!:string;
 
   ngOnInit() {
-    this.title='Pichu'
-    this.description='Petite souris jaune électrique'
-    this.creatDate=new Date();
-    this.snaps=25;
-    this.imageUrl='https://www.pokebip.com/pokedex-images/ecarlate-violet/300/172.png?v=ev2'
+    this.buttonText='Oh Snap!';
 
-    /*this.title2='Pikachu'
-    this.description2='Évolution de Pichu'
-    this.creatDate2=new Date();
-    this.snaps2=32;
-    this.imageUrl2='https://www.pokebip.com/pokedex-images/ecarlate-violet/300/25.png?v=ev2'
-
-    this.title3='Raichu'
-    this.description3='Évolution de Pikachu'
-    this.creatDate3=new Date();
-    this.snaps3=50;
-    this.imageUrl3='https://www.pokebip.com/pokedex-images/ecarlate-violet/300/26.png?v=ev2'*/
   }
 
-  onAddSnap(){
-    this.snaps ++;
+  onSnap(){
+    if(this.buttonText==='Oh Snap!'){
+      this.faceSnap.snaps++;
+      this.buttonText='Oups, unSnap!'
+
+    }
+    else{
+      this.faceSnap.snaps--;
+      this.buttonText='Oh Snap!'
+    }
+
   }
 
   onRazSnaps(){
-    this.snaps=25;
+    this.faceSnap.snaps=25;
   }
 
 
