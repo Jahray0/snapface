8e79769fcd80778ed4b458017944fe856764e907
Jahray0 <jaegle.maxime@gmail.com>
2023-07-01 10:35:37 +0200

    v 1.3.6 route via methode

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent {
+
+  //vu que onContinue est comme un service on peut le mettre dans le constructeur comme argument (router)
+  constructor(private router:Router) { }
+
+  ngOnInit(): void {}
+
+  onContinue() :void {
+    this.router.navigateByUrl('facesnaps')
+  }
 
 }
