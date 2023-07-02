7ea99ce9cd97906990879d49ddd3a59f1b38378f
Jahray0 <jaegle.maxime@gmail.com>
2023-07-02 15:49:42 +0200

    changement de app.component.ts pour les opérateurs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, filter, interval, map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+
+  interval$! : Observable<string>;
 
 
 
@@ -17,8 +20,24 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
 
+    this.interval$ = interval(1000).pipe(
+      /*On ne laisse que passer les valeurs divisibles par 3*/
+      filter(value => value % 3 === 0),
+      map(value => value%2 === 0 ?
+        /*n'emet plus de number mais des sting*/
+        `je suis ${value} et je suis pair`:
+        `je suis ${value} et je suis impair`
+        ),
+        tap(text=>this.logger(text))
+    );
 
-    /*this.mySnap = {
+
+  }
+  logger(text:string){
+    console.log(`log: ${text}`);
+  }
+
+    /*this.mySnap = {    => normalement dans la section ngOnInit
       title: 'Pichu',
       description: 'Petite souris jaune électrique',
       creatDate: new Date(),
@@ -48,7 +67,6 @@ export class AppComponent implements OnInit {
 
     /*this.mySnap2=new FaceSnap('Pikachu', 'Évolution de Pichu', new Date(), 25, 'https://www.pokebip.com/pokedex-images/ecarlate-violet/300/25.png?v=ev2');
     this.mySnap3=new FaceSnap('Raichu', 'Évolution de Raichu', new Date(), 25, 'https://www.pokebip.com/pokedex-images/ecarlate-violet/300/26.png?v=ev2');*/
-  }
 
   title = '';
 
