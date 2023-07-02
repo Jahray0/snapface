c81fe8abf72a2232683ed26a484da525f69aa097
Jahray0 <jaegle.maxime@gmail.com>
2023-07-02 19:55:06 +0200

    v 1.5.2.2 apercu

--- src/app/new-face-snap/new-face-snap.component.ts
+++ src/app/new-face-snap/new-face-snap.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { FaceSnap } from '../models/face-snap.model';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-new-face-snap',
@@ -9,6 +11,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class NewFaceSnapComponent implements OnInit{
 
   snapForm!: FormGroup;
+  faceSnapPreview$!: Observable<FaceSnap>;
 
   constructor(private formBuilder:FormBuilder) { }
 
@@ -19,6 +22,16 @@ export class NewFaceSnapComponent implements OnInit{
       imageUrl:[null],
       location:[null],
     });
+
+    this.faceSnapPreview$ = this.snapForm.valueChanges.pipe(
+      map(formValue => ({
+          ...formValue,
+          creatDate: new Date(),
+          snaps: 0,
+          id: 0
+      }))
+  );
+
   }
 
   onSubmitForm(): void {
