d9728b44ae15619b07587252021cb1d0782e3595
Jahray0 <jaegle.maxime@gmail.com>
2023-07-02 16:23:13 +0200

    v 1.5.1 unsubscribe

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapsService } from '../services/face-snaps.service';
+import { Subject, interval, takeUntil, tap } from 'rxjs';
 
 
 @Component({
@@ -12,12 +13,23 @@ import { FaceSnapsService } from '../services/face-snaps.service';
 export class FaceSnapListComponent implements OnInit {
 
   faceSnaps!: FaceSnap[];
+  private destroy$! : Subject<boolean>;
 
   //On injecte le service dans le constructeur
   constructor(private faceSnapsService:FaceSnapsService) { }
 
   ngOnInit(): void {
+    this.destroy$ = new Subject<boolean>();
     this.faceSnaps = this.faceSnapsService.getAllFaceSnaps();
+
+    interval(1000).pipe(
+      takeUntil(this.destroy$),
+      tap(console.log)
+    ).subscribe();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
   }
 
 }
