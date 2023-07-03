6b205c5228f66d6d9bf419d762f851aeb79cede6
Jahray0 <jaegle.maxime@gmail.com>
2023-07-03 15:29:46 +0200

    v 1.5.8 authentification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { SingleFaceSnapComponent } from './single-face-snap/single-face-snap.com
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NewFaceSnapComponent } from './new-face-snap/new-face-snap.component';
 import { HttpClientModule } from '@angular/common/http';
+import { httpInterceptorProviders } from './Interceptor';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import { HttpClientModule } from '@angular/common/http';
 
   ],
   providers: [
-    { provide: LOCALE_ID, useValue: 'fr-FR'}
+    { provide: LOCALE_ID, useValue: 'fr-FR'},
+    httpInterceptorProviders
   ],
   bootstrap: [AppComponent]
 })
