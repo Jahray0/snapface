b45b0860f4471f15d719811d1a6132a5289dce53
Jahray0 <jaegle.maxime@gmail.com>
2023-07-03 11:45:54 +0200

    v 1.5.6 requete simple

--- src/app/services/face-snaps.service.ts
+++ src/app/services/face-snaps.service.ts
@@ -15,81 +15,20 @@ export class FaceSnapsService {
   
   constructor(private http: HttpClient) {} //Injection de dépendance : le service a besoin d'un autre service (ici httpClient)
 
-    faceSnaps: FaceSnap[] = [{
-        id: 1,
-        title: 'Pichu',
-        description: 'Petite souris jaune électrique',
-        createdDate: new Date(),
-        snaps: 25,
-        imageUrl: 'https://www.pokebip.com/pokedex-images/ecarlate-violet/300/172.png?v=ev2',
-        location: 'Jardin Trophée'
-      },
-      {
-        id: 2,
-        title: 'Pikachu',
-        description: 'Évolution de Pichu',
-        createdDate: new Date(),
-        snaps: 32,
-        imageUrl: 'https://www.pokebip.com/pokedex-images/ecarlate-violet/300/25.png?v=ev2',
-        location: 'Forêt de Jade'
-      },
-      {
-        id: 3,
-        title: 'Raichu',
-        description: 'Évolution de Pikachu',
-        createdDate: new Date(),
-        snaps: 50,
-        imageUrl: 'https://www.pokebip.com/pokedex-images/ecarlate-violet/300/26.png?v=ev2',
-        location: `Plage de l'Épreuve`
-      },
-    
-      {
-        id: 4,
-        title: 'Salameche',
-        description: 'Salamandre de feu',
-        createdDate: new Date(),
-        snaps: 25,
-        imageUrl: 'https://www.pokebip.com/pokedex-images/ecarlate-violet/300/4.png?v=ev2',
-        location: 'Bourg Palette'
-      },
-    
-      {
-        id: 5,
-        title: 'Reptincel',
-        description: 'Évolution Salameche',
-        createdDate: new Date(),
-        snaps: 32,
-        imageUrl: 'https://www.pokebip.com/pokedex-images/ecarlate-violet/300/5.png?v=ev2',
-        location: 'Batisques'
-      },
-    
-      {
-        id: 6,
-        title: 'Dracaufeu',
-        description: 'Évolution Reptincel',
-        createdDate: new Date(),
-        snaps: 50,
-        imageUrl: 'https://www.pokebip.com/pokedex-images/ecarlate-violet/300/6.png?v=ev2',
-        location: 'Antre 114'
-      }];
+    faceSnaps: FaceSnap[] = [];
 
       getAllFaceSnaps(): Observable<FaceSnap[]> {
         return this.http.get<FaceSnap[]>('http://localhost:3000/facesnaps');
     }
 
-      getFaceSnapById(faceSnapId: number): FaceSnap {
-        const faceSnap = this.faceSnaps.find(faceSnap => faceSnap.id === faceSnapId);
-        if (!faceSnap) {
-            throw new Error('FaceSnap not found!');
-            }else{
-                return faceSnap;
-            }
+      getFaceSnapById(faceSnapId: number): Observable<FaceSnap> {
+        return this.http.get<FaceSnap>(`http://localhost:3000/facesnaps/${faceSnapId}`);
       }
 
       //type litéral : snapType peut prendre 2 valeurs : snap ou unsnap
       snapFaceSnapById(faceSnapId: number, snapType: 'snap' | 'unsnap'): void { 
-        const faceSnap = this.getFaceSnapById(faceSnapId);
-        snapType === 'snap' ? faceSnap.snaps++ : faceSnap.snaps--;
+        //const faceSnap = this.getFaceSnapById(faceSnapId);
+        //snapType === 'snap' ? faceSnap.snaps++ : faceSnap.snaps--;
       }
 
       addFaceSnap(formValue: FaceSnap): void {
